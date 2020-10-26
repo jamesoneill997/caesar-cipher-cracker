73dd74ebcd3de9e2debc7bf530105dad529a3969
jamesoneill997 <jamesoneill997@gmail.com>
2020-10-26 22:26:22 +0000

    added frequent letters and tracked occurrances of letters

--- caesar.py
+++ caesar.py
@@ -1,16 +1,24 @@
 import string
 
 alphabet_string = list(string.ascii_lowercase)
+count = [0 for i in range(26)]
+occurrances = dict(zip(alphabet_string, count))
+
+frequent = {'e':4, 't':19, 'a':0, 'o':13, 'i':8, 'n':14}
 
 #ZWPFLYRMVJLTTVJJWLCCPUVTIPGKVUKYZJDVJJ RXVLJZEXRSILKVWFITVRKKRTBKYVEKIPUVTIPGKZEX ZKLJZEXRWIVHLVETPRERCPJZJRKKRTBZEJKVRU 
 
 def crack(cipher):
     cipher = cipher.lower().replace(' ','')
-    with open("output.txt", "a") as myfile:
-        for i in range(25):
-            for letter in cipher:
-                myfile.write(alphabet_string[alphabet_string.index(letter) - i].upper())
-            myfile.write('\n')
+
+    for letter in cipher:
+        occurrances[letter] += 1
+
+    top_3 = sorted(occurrances, key=occurrances.get, reverse=True)[:3]
+
+    indices = [alphabet_string.index(top_3[i]) for i in range(len(top_3))]
+
+    with open('output.txt', 'a') as my_file:
 
 
 crack('ZWPFLYRMVJLTTVJJWLCCPUVTIPGKVUKYZJDVJJ RXVLJZEXRSILKVWFITVRKKRTBKYVEKIPUVTIPGKZEX ZKLJZEXRWIVHLVETPRERCPJZJRKKRTBZEJKVRU')
